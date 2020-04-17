5a8baf438f701674c9c627b4913be8ac72c3e7b3
Celso Inácio <celso.almeida@seredesa.com.br>
2020-04-17 17:55:04 -0300

    fix bundle

--- bundle.js
+++ bundle.js
@@ -1,13 +1,3 @@
-const ENV_DEV = true
-
-// import '../assets';
-
-if(ENV_DEV){
-    require('../build/css/bootstrap.min.css');
-    require('../build/css/styles.css');
-}
-
-
 document.addEventListener('readystatechange', event => { 
     if (event.target.readyState === "complete") {
         document.querySelector(".bar").classList.add("bar-ready");
