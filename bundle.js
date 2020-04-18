5f954d9283db1b2b5d9a2a51c12a7e3551c1d0fc
Celso Inácio <celso.almeida@seredesa.com.br>
2020-04-18 12:07:26 -0300

    Site contact form and clients

--- bundle.js
+++ bundle.js
@@ -5,3 +5,32 @@ document.addEventListener('readystatechange', event => {
         document.querySelector(".lead").classList.add("lead-ready");
     }
 });
+
+(function(){
+    emailjs.init("user_We81RgxANFr6lOQvkkgdi");
+ })();
+
+
+ function onSubmit(token) {
+
+ }
+
+ function sendMail(){
+    document.querySelector('.contactform-submit').value = "Enviando..."
+    grecaptcha.execute();
+    var templateParams = {
+        from_name: document.querySelector('#nome').value,
+        emailname: document.querySelector('#email').value,
+        to_name: 'Enssure',
+        message_html: document.querySelector('#mensagem').value
+    };
+     
+    emailjs.send('gmail', 'template_GfPemw7o', templateParams)
+    .then(function(response) {
+        document.querySelector('.contactform-submit').value = "Mensagem enviada!"
+        console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+        document.querySelector('.contactform-submit').value = "Falha ao enviar"
+        console.log('FAILED...', error);
+    });
+ }
